79a7a113e9bf1ca1a7534ffd02f81a34028bc3e1
Lorenzo Macias <lzomacias@gmail.com>
2024-12-11 19:36:32 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+// initializes variables
+const value1 = 5 * 2;
+const value2 = 80 / 2;
+const value3 = 40 - 1;
+
+// creates alert dialog box with combination for end user
+alert(value1 + " - " + value2 + "- " + value3)
